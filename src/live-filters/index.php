766498046aa9967accf95f78cd9f019affa0b1fa
Morgan Leek <morganleek@gmail.com>
2025-12-13 17:40:33 +0800

    hide empty toggle

--- src/live-filters/index.php
+++ src/live-filters/index.php
@@ -4,5 +4,6 @@
 <div class="wp-block-scm-live-filters"
 	filters='<?php print wp_json_encode( $filterLabels ); ?>'
 	multiSelect='<?php print $multiSelect ? 1 : 0; ?>'
+	hideEmpty='<?php print $hideEmpty ? 1 : 0; ?>'
 	layout='<?php print $layout; ?>'
 ></div>
